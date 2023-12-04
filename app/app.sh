3223787b97ee43612d9c8fac95eba0dc9b829863
The XOR <1h3x0r@gmail.com>
2023-12-04 09:17:41 +0100

    3

--- app/app.sh
+++ app/app.sh
@@ -8,7 +8,7 @@ bail()
 
 source /app/imagestart.sh
 
-#exec websockify --web=/usr/share/novnc/ 8080 172.17.0.1:5901
+exec websockify --web=/usr/share/novnc/ 8080 192.168.100.18:5901
 
 /app/biglybtcheck.sh
 
